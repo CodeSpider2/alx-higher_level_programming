fdf70e28068c8f30b2696b31aa67ed6552df20ee
AMO15310 <wachiraamos402@gmail.com>
2023-03-10 12:18:31 +0300

    Upload  argsv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    num = len(sys.argv)- 1
+    from sys import argv
+    
+    num = len(argv)- 1
     if num == 0:
         print("0 arguments")
-    elif num = 1:
+    elif num == 1:
         print("1 arguments")
     else:
         print("{} arguments:".format(num))
