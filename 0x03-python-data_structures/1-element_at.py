f82d36293b02a5c4b513284dc419a3fae2a3ace8
AMO15310 <wachiraamos402@gmail.com>
2023-03-11 22:29:08 +0300

    Retrieve number at index idx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if (idx) < 0:
-        return 0
-    elif idx > len(my_list):
-        return 0
+        return None
+    elif idx > len(my_list)-1:
+        return None
     else:
-        print("Element at index {} is {}".format(idx,my_list[idx]))
+        print("Element at index {} is {}".format(idx, my_list[idx]))
