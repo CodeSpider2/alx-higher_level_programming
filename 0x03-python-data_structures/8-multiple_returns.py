cb20cef7b8c531287ecc6c3fc769461d09e043bf
AMO15310 <wachiraamos402@gmail.com>
2023-03-12 11:09:34 +0300

    Return a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if !sentence[0]:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    return tuple(first_char, len(sentence))
