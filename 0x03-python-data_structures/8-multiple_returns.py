4d547239c96702ccf1ed0b502ed2218e2668b716
AMO15310 <wachiraamos402@gmail.com>
2023-03-12 11:23:02 +0300

    Return a tupler

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence[0]= "":
+    if sentence[0]== "":
         first_char = None
+        return (0, first_char)
     else:
         first_char = sentence[0]
-    return (len(sentence), first_char)
+        return (len(sentence), first_char)
