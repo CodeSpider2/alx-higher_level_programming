14126730b81c248ca8789021c41a8164c0e5bd6d
AMO15310 <wachiraamos402@gmail.com>
2023-03-12 11:20:52 +0300

    Return a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if !sentence[0]:
+    if sentence[0]= "":
         first_char = None
     else:
         first_char = sentence[0]
-    return tuple(first_char, len(sentence))
+    return (len(sentence), first_char)
