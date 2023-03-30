9ee9c43b369122b46113aa41cbc0535f3c96f35a
AMO15310 <wachiraamos402@gmail.com>
2023-03-30 15:04:17 +0300

    Initialize a new square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Define a square """
+
+
+class Square:
+    """Represent a square """
+    
+    def __init__(self, size):
+
+        """
+        Initializing a square
+
+        Args: size(int) - Size of the new square
+
+        """
+        self.__size = size
