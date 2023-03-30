e4bcc0c53e35be031f76ae25b8b89296c5936eae
AMO15310 <wachiraamos402@gmail.com>
2023-03-30 15:24:14 +0300

    Check the input size value

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" Define a square """
+# 2-square.py
+
+"""Define a class Square."""
 
 
 class Square:
-    """Represent a square """
-    
+    """Represent a square."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
-        Initializing a square.
-
-        Args: size(int) : Size of the new square
-
-        """
-
         if not isinstance(size, int):
-            raise TypeError("size must be an integer ")
-
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        
         self.__size = size
