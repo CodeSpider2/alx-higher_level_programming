75dbb633313f0fca6f2b163686a68ed3e0c92c96
AMO15310 <wachiraamos402@gmail.com>
2023-04-11 23:43:46 +0300

    Area 501

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#Amos Wachira
+
+"""
+This module provides an empty class Base Geometry.
+"""
+
+
+class BaseGeometry:
+    """Represents base geometry."""
+
+    def area:
+        """Raises:
+            exception:Area is not defined
+            """
+        raise Exception("area() is not implemented")
