13032db5107aa5f53cfdc18098e7b1d0d872f16e
AMO15310 <wachiraamos402@gmail.com>
2023-04-11 23:38:40 +0300

    empty class basegeometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #Amos Wachira
-"""Define an empty class"""
+"""Define an empty class Base Geometry"""
 
 class BaseGeometry:
-    """Empty class BaseGeometry"""
+    """Empty class to represent Base Geometry"""
     pass
