29c8e68f70ae687bbfaa35230b492b550697de96
AMO15310 <wachiraamos402@gmail.com>
2023-04-12 20:59:34 +0300

    Write json to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#Amos Wachira
+"""Define a function in this module to save a text file as json"""
+
+def save_to_json_file(my_obj, filename):
+    """
+    Args: 
+        my_obj:Saves my this object to the filename argument as json
+        filename:The file to be saved to
+    Returns:
+            void
+    """
+    import json
+    with open(filename, mode="w", encode="utf-8") as file:
+        json.dump(my_obj, file)
